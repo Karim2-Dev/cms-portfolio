40468a562f36ffcc37a93bfdb56117f29f7d4f72
Karim Samer <karim.samer19@gmail.com>
2026-06-30 19:35:23 +0300

    add bottom section

--- components/mobileNav/MobileNav.tsx
+++ components/mobileNav/MobileNav.tsx
@@ -1,8 +1,24 @@
 import { navLinks } from "@/lib/links";
+import Link from "next/link";
+import { HiMiniPlusSmall, HiPlus } from "react-icons/hi2";
 export default function MobileNav() {
   return (
-    <div className="flex md:hidden fixed bottom-0 left-0 right-0 z-50 bg-red-600 h-17 w-full">
-      <div className="container">test</div>
+    <div className="flex md:hidden fixed bottom-0 left-0 right-0 z-50 bg-background border-t h-17 w-full">
+      <div className="container flex items-center justify-center gap-7 py-2 px-2 ">
+        {navLinks.map(({ href, label, segment, icon: Icon }) => (
+          <Link
+            href={href}
+            key={segment}
+            className="flex flex-col items-center justify-center gap-1 text-sm hover:text-primary transition-colors"
+          >
+            <Icon className="w-5 h-5" />
+            {label}
+          </Link>
+        ))}
+        <button className="flex items-center justify-center p-2 rounded-full bg-primary text-background hover:bg-primary/90 transition-colors">
+          <HiPlus className="w-5 h-5 shrink-0" />
+        </button>
+      </div>
     </div>
   );
 }
